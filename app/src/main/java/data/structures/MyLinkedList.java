94e5b537545f9e23b045f075a86b31310d938d35
Kiwi Bui <kiwibui4@gmail.com>
2021-05-04 11:04:11 -0700

    Completed linked list

--- app/src/main/java/data/structures/MyLinkedList.java
+++ app/src/main/java/data/structures/MyLinkedList.java
@@ -1,11 +1,13 @@
 package data.structures;
 
+@SuppressWarnings("DuplicatedCode")
 public class MyLinkedList {
+
     public class Node {
         Node next;
-        int data;
+        Object data;
 
-        public Node(int data) {
+        public Node(Object data) {
             this.data = data;
         }
     }
@@ -16,9 +18,10 @@ public class MyLinkedList {
         return (head == null) ? true : false;
     }
 
-    public void add(int data) {
+    public void add(Object data) {
         if (head == null) {
             head = new Node(data);
+            return;
         }
 
         Node current = head;
@@ -29,15 +32,80 @@ public class MyLinkedList {
         current.next = new Node(data);
     }
 
-    public int get(int i) {
+    public Object get(int nth) {
         Node current = head;
 
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < nth - 1; j++) {
             current = current.next;
         }
 
         try {
             return current.data;
+        } catch (NullPointerException e) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public Object getFirst() {
+        return head.data;
+    }
+
+    public Object getLast() {
+        Node current = head;
+
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        return current.data;
+    }
+
+    public int contains(Object object) {
+        Node current = head;
+        int index = 0;
+
+        while (current != null) {
+            if (current.data.equals(object)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+
+        return -1;
+    }
+
+
+    public void delete(Object object) {
+        if (head.data.equals(object)) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+
+        while (current != null) {
+            if (current.next.data.equals(object)) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public void delete(int nth) {
+        if (nth == 1) {
+            head = head.next;
+            return;
+        }
+        Node current = head;
+
+        try {
+            for (int j = 0; j < nth - 2; j++) {
+                current = current.next;
+            }
+
+            current.next = current.next.next;
         } catch (NullPointerException e) {
             throw new IndexOutOfBoundsException();
         }
--- app/src/test/java/data/structures/MyLinkedListTest.java
+++ app/src/test/java/data/structures/MyLinkedListTest.java
@@ -20,7 +20,7 @@ public class MyLinkedListTest {
 
         myLinkedList.add(4);
 
-        assertEquals("Linked list should be empty", false, myLinkedList.isEmpty());
+        assertEquals("Linked list should not be empty", false, myLinkedList.isEmpty());
     }
 
     @Test
@@ -30,7 +30,7 @@ public class MyLinkedListTest {
         myLinkedList.add(4);
         myLinkedList.add(6);
 
-        assertEquals("Linked list should be empty", 6, myLinkedList.get(2));
+        assertEquals("Should get 2nd element", 6, myLinkedList.get(2));
     }
 
     @Test
@@ -43,7 +43,76 @@ public class MyLinkedListTest {
         try {
             myLinkedList.get(3);
             fail("Did not throw IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            assertEquals("Should throw IndexOutOfBoundsException", IndexOutOfBoundsException.class,
+                    e.getClass());
+        }
+    }
 
+    @Test
+    public void getFirstTest() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add(4);
+        myLinkedList.add(6);
+
+        assertEquals("Should get first element", 4, myLinkedList.getFirst());
+    }
+
+    @Test
+    public void getLastTest() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add(4);
+        myLinkedList.add(6);
+
+        assertEquals("Should get last element", 6, myLinkedList.getLast());
+    }
+
+    @Test
+    public void containsTest() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add(5);
+        myLinkedList.add(6);
+        myLinkedList.add(8);
+
+        assertEquals("Should index of object", 1, myLinkedList.contains(6));
+    }
+
+    @Test
+    public void deleteTest() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add("First");
+        myLinkedList.add("Second");
+        myLinkedList.delete("Second");
+        myLinkedList.delete("First");
+
+        assertEquals("Should delete and make linked list empty", true, myLinkedList.isEmpty());
+    }
+
+    @Test
+    public void deleteNthTest() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add("First");
+        myLinkedList.add("Second");
+        myLinkedList.delete(2);
+
+        assertEquals("Linked list should not contain 'test'", -1, myLinkedList.contains("Second"));
+    }
+
+    @Test
+    public void deleteNthExceptionTest() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add("First");
+        myLinkedList.add("Second");
+
+        try {
+            myLinkedList.delete(3);
+            fail("Did not throw IndexOutOfBoundsException");
         } catch (IndexOutOfBoundsException e) {
             assertEquals("Should throw IndexOutOfBoundsException", IndexOutOfBoundsException.class,
                     e.getClass());
