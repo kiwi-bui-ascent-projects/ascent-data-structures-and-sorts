8ea9c6f63c6d1966b5bfceda6ef64fa7c4f28a44
Kiwi Bui <kiwibui4@gmail.com>
2021-05-04 09:27:17 -0700

    Implemented add and get methods for LinkedList

--- app/src/main/java/data/structures/MyLinkedList.java
+++ app/src/main/java/data/structures/MyLinkedList.java
@@ -12,8 +12,34 @@ public class MyLinkedList {
 
     Node head;
 
-    public Object isEmpty() {
+    public boolean isEmpty() {
         return (head == null) ? true : false;
     }
 
+    public void add(int data) {
+        if (head == null) {
+            head = new Node(data);
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = new Node(data);
+    }
+
+    public int get(int i) {
+        Node current = head;
+
+        for (int j = 0; j < i; j++) {
+            current = current.next;
+        }
+
+        try {
+            return current.data;
+        } catch (NullPointerException e) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
 }
--- app/src/test/java/data/structures/MyLinkedListTest.java
+++ app/src/test/java/data/structures/MyLinkedListTest.java
@@ -1,7 +1,10 @@
 package data.structures;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 
 public class MyLinkedListTest {
     @Test
@@ -12,9 +15,38 @@ public class MyLinkedListTest {
     }
 
     @Test
-    public void () {
+    public void canAddFirstTest() {
         MyLinkedList myLinkedList = new MyLinkedList();
 
-        assertEquals("New linked list should be empty", true, myLinkedList.isEmpty());
+        myLinkedList.add(4);
+
+        assertEquals("Linked list should be empty", false, myLinkedList.isEmpty());
+    }
+
+    @Test
+    public void canGetTest() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add(4);
+        myLinkedList.add(6);
+
+        assertEquals("Linked list should be empty", 6, myLinkedList.get(2));
+    }
+
+    @Test
+    public void getThrowExceptionTest() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add(4);
+        myLinkedList.add(6);
+
+        try {
+            myLinkedList.get(3);
+            fail("Did not throw IndexOutOfBoundsException");
+
+        } catch (IndexOutOfBoundsException e) {
+            assertEquals("Should throw IndexOutOfBoundsException", IndexOutOfBoundsException.class,
+                    e.getClass());
+        }
     }
 }
